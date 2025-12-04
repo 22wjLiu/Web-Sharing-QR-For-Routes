b4fe57eef5714eb9710bd987b651167eeeb453d6
liuwanjia <wanyaba@outlook.com>
2025-12-04 22:30:45 +0800

    feat(backend): :sparkles: 初始化后端项目

---
+++ backend/QRForRoutes/src/main/java/com/liu/qrRoutes/QrForRoutesApplication.java
@@ -0,0 +1,13 @@
+package com.liu.qrRoutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrForRoutesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(QrForRoutesApplication.class, args);
+  }
+
+}
---
+++ backend/QRForRoutes/src/test/java/com/liu/qrRoutes/QrForRoutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liu.qrRoutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrForRoutesApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
