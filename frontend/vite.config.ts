4e050a247ac0d2ed02b277a342c9af82660d979c
liuwanjia <wanyaba@outlook.com>
2025-12-06 22:35:38 +0800

    chore(frontend): :hammer: 添加开发和生产环境全局变量、添加新依赖和vite代理
    添加开发和生产环境全局变量、添加新依赖和vite代理

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -21,4 +21,17 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+      '/static': {
+        target: 'http://localhost:8080/static',
+        changeOrigin: true,
+      },
+    },
+  },
 });
